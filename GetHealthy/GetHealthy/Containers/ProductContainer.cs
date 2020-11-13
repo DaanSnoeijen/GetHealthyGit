8be6427610577777ab5db3f48e39454fe487cd31
Daan Snoeijen <438919@student.fontys.nl>
2020-11-13 13:41:33 +0100

    Netter gemaakt

--- GetHealthy/GetHealthy/Containers/ProductContainer.cs
+++ GetHealthy/GetHealthy/Containers/ProductContainer.cs
@@ -7,6 +7,7 @@ namespace GetHealthy.Controllers
     class ProductContainer
     {
         public List<Product> ProductList = new List<Product>();
+        public List<Product> AddedProducts = new List<Product>();
 
         public void GetProduct()
         {
