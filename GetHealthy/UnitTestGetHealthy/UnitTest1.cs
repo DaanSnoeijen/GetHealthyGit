1f3762afffe864eca37e7b680abdfc71652571e0
Daan Snoeijen <438919@student.fontys.nl>
2020-11-23 09:57:12 +0100

    Unit test toegevoegd

---
+++ GetHealthy/UnitTestGetHealthy/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestGetHealthy
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arange
+            
+
+            //Act
+            //Assert
+        }
+    }
+}
